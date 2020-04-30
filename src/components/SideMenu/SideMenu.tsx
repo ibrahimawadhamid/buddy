f77c56e71c5c2c91006b8be8aab100344fcb9cf1
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2020-05-01 00:47:56 +0200

    code clean up

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -9,7 +9,6 @@ import {
   IonMenuToggle,
   IonNote,
   IonAvatar,
-  IonButton,
   IonCol,
   IonGrid,
   IonRow,
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import {
   IonPage,
-  IonHeader,
-  IonToolbar,
   IonButtons,
   IonMenuButton,
-  IonTitle,
   IonContent,
   IonGrid,
   IonRow,
   IonCol,
-  IonItem,
   IonLabel,
 } from "@ionic/react";
 import { useTranslation } from "react-i18next";
@@ -22,28 +18,14 @@ const Home: React.FC = () => {
 
   return (
     <IonPage>
-      {/* <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{t("Home")}</IonTitle>
-        </IonToolbar>
-      </IonHeader> */}
-
       <IonContent>
         <div className="fake-header">
           <IonButtons slot="start">
             <IonMenuButton color="light" />
-            <IonLabel className="main-title ion-margin-start">Home</IonLabel>
+            <IonLabel className="main-title ion-margin-start">
+              {t("Home")}
+            </IonLabel>
           </IonButtons>
-
-          {/* <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-            <IonTitle>{t("Home")}</IonTitle>
-          </IonToolbar> */}
         </div>
         <IonGrid>
           <IonRow>
