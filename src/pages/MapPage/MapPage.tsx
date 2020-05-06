e58aa89925c5d2d54786e75242d5894e7baaa58c
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2020-05-06 22:36:18 +0200

    added dummy map

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,30 +1,60 @@
-import React from "react";
-import { IonPage, IonContent, IonGrid, IonRow, IonCol } from "@ionic/react";
+import React, { useEffect, useRef } from "react";
+import {
+  IonPage,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonHeader,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import { useTranslation } from "react-i18next";
-import { Map, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 import "./MapPage.css";
-import PageHeader from "../../components/PageHeader/PageHeader";
 
 const MapPage: React.FC = () => {
   const { t } = useTranslation();
+  const mapRef = useRef(null);
+
+  /**
+   * Run only the first time this component loads
+   */
+  useEffect(() => {
+    L.map("mapDiv", {
+      center: [25, 30],
+      zoom: 4,
+      layers: [
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a target="_blank" href="https://buddy-ionic.web.app">Buddy</a>',
+        }),
+      ],
+    });
+    setTimeout(function () {
+      window.dispatchEvent(new Event("resize"));
+    }, 1000);
+  }, []);
+
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar color="success" className="map-page-header">
+          <IonButtons>
+            <IonMenuButton slot="start" />
+            <IonTitle>{t("Map")}</IonTitle>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
       <IonContent>
-        <PageHeader
-          title={t("Map")}
-          backButtonEnabled={false}
-          menuButtonEnabled={true}
-        />
         <IonGrid>
           <IonRow>
             <IonCol>
-              <Map id="mapDiv" center={[25, 45]} zoom={4}>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                ></TileLayer>
-              </Map>
+              <div ref={mapRef} id="mapDiv"></div>
             </IonCol>
           </IonRow>
         </IonGrid>
